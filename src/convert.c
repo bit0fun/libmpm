ffb1d26966775e62443dca0d1783111e3bc87484
Dylan Wadler <dwadler@ips-yes.com>
2019-02-07 17:35:23 -0500

    Starting to add type conversion functions
    Prototypes for type conversions finished. Beginning to write out
    functions in convert.c

--- src/convert.c
+++ src/convert.c
@@ -1,60 +1,109 @@
-/*
- * ISC License
- * 
- * Copyright (c) 2019, Dylan Wadler
- * 
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- *
- */
-
-#ifndef MPM_CONVERT_H
-#define MPM_CONVERT_H
-
-#include "mpm_types.h"
+#include "convert.h"
 
 /** Double Fixed Point Type Conversions **/
 
 /* Converting to Double Fixed Point */
-struct dfp_t int2dfp( int i );
-struct dfp_t float2dfp( float f );
-struct dfp_t double2dfp( double d );
-struct dfp_t uint2dfp( unsigned int u );
+struct dfp_t int2dfp( int i ){
 
-struct scidfp_t int2sci( int i );
-struct scidfp_t float2sci( float f );
-struct scidfp_t double2sci( double d );
-struct scidfp_t uint2sci( unsigned int u );
+}
 
-struct polardfp_t int2polar( int i );
-struct polardfp_t float2polar( float f );
-struct polardfp_t double2polar( double d );
-struct polardfp_t uint2dfp( unsigned int u );
+struct dfp_t float2dfp( float f ){
 
-struct cdfp_t int2complex( int i );
-struct cdfp_t float2complex( float f );
-struct cdfp_t double2complex( double d );
-struct cdfp_t uint2comlex( unsigned int u );
+}
+
+struct dfp_t double2dfp( double d ){
+
+}
+
+struct dfp_t uint2dfp( unsigned int u ){
+
+}
+
+
+struct scidfp_t int2sci( int i ){
+
+}
+
+struct scidfp_t float2sci( float f ){
+
+}
+
+struct scidfp_t double2sci( double d ){
+
+}
+
+struct scidfp_t uint2sci( unsigned int u ){
+
+}
+
+
+struct polardfp_t int2polar( int i ){
+
+}
+
+struct polardfp_t float2polar( float f ){
+
+}
+
+struct polardfp_t double2polar( double d ){
+
+}
+
+struct polardfp_t uint2dfp( unsigned int u ){
+
+}
+
+
+struct cdfp_t int2complex( int i ){
+
+}
+
+struct cdfp_t float2complex( float f ){
+
+}
+
+struct cdfp_t double2complex( double d ){
+
+}
+
+struct cdfp_t uint2comlex( unsigned int u ){
+
+}
+
 
 /* Converting from Double Fixed Point */
-int 			dfp2int( struct dfp_t dfp );
-float 			dfp2float( struct dfp_t dfp );
-double 			dfp2double( struct dfp_t dfp );
-unsigned int 	dfp2uint( struct dfp_t dfp );
+int	dfp2int( struct dfp_t dfp ){
 
-int 			sci2int( struct scidfp_t sci );
-float 			sci2float( struct scidfp_t sci );
-double 			sci2double( struct scidfp_t sci );
-unsigned int 	sci2uint( struct scidfp_t sci );
+}
+
+float dfp2float( struct dfp_t dfp ){
+}
+
+double dfp2double( struct dfp_t dfp ){
+
+}
+
+unsigned int dfp2uint( struct dfp_t dfp ){
+
+}
+
+
+int	sci2int( struct scidfp_t sci ){
+
+}
+
+float sci2float( struct scidfp_t sci ){
+
+}
+
+double sci2double( struct scidfp_t sci ){
+
+}
+
+unsigned int sci2uint( struct scidfp_t sci ){
+
+}
+
 
 int 			polar2int( struct polardfp_t polar );
 float 			polar2float( struct polardfp_t polar );
@@ -162,5 +211,3 @@ struct cdfp_t sym2complex( struct sym_t sym );
 struct cdfp_t psym2complex( struct psym_t psym );
 struct cdfp_t csym2complex( struct csym_t csym );
 
-
-#endif /* MPM_CONVERT_H */
